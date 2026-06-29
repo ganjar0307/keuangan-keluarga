2b6bc6bcc3461077c9be3a2bcfc391387147eb8e
ganjar0307 <95399701+ganjar0307@users.noreply.github.com>
2026-06-29 18:51:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+
+const API_URL='PASTE_WEBAPP_URL_HERE';
+
+let user='';
+
+function login(){
+let u=username.value;
+let p=password.value;
+
+fetch(API_URL+"?action=login&username="+u+"&password="+p)
+.then(r=>r.json())
+.then(data=>{
+if(data.success){
+localStorage.setItem("user",u);
+user=u;
+showApp();
+}else{
+alert("Login gagal");
+}
+});
+}
+
+function showApp(){
+user=localStorage.getItem("user");
+if(!user)return;
+loginBox.style.display='none';
+app.style.display='block';
+welcome.innerHTML='Halo '+user;
+loadData();
+}
+
+function simpan(){
+fetch(API_URL,{
+method:'POST',
+body:JSON.stringify({
+action:'save',
+user:user,
+tanggal:tanggal.value,
+jenis:jenis.value,
+kategori:kategori.value,
+keterangan:keterangan.value,
+nominal:nominal.value
+})
+})
+.then(r=>r.json())
+.then(()=>loadData());
+}
+
+function loadData(){
+fetch(API_URL+"?action=get")
+.then(r=>r.json())
+.then(data=>{
+let html='';
+data.forEach(d=>{
+html+=`<tr>
+<td>${d.tanggal}</td>
+<td>${d.user}</td>
+<td>${d.jenis}</td>
+<td>${d.kategori}</td>
+<td>${d.nominal}</td>
+</tr>`;
+});
+tbl.querySelector('tbody').innerHTML=html;
+});
+}
+
+function logout(){
+localStorage.clear();
+location.reload();
+}
+
+showApp();
