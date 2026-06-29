52e727ace4fdbc746e47c05bbebb69f442d70025
ganjar0307 <95399701+ganjar0307@users.noreply.github.com>
2026-06-29 19:11:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+const API_URL='PASTE_WEBAPP_URL_HERE';
+
+const kategoriData={
+Pengeluaran:['🍔 Makanan','🛒 Belanja','🚗 Transportasi','💡 Tagihan'],
+Pemasukan:['💼 Gaji','💰 Bonus','💳 Transfer']
+};
+
+function kategoriUpdate(){
+kategori.innerHTML='';
+
+kategoriData[jenis.value].forEach(item=>{
+kategori.innerHTML += `<option>${item}</option>`;
+});
+}
+
+kategoriUpdate();
+
+function login(){
+
+fetch(
+API_URL+
+'?action=login&username='+
+username.value+
+'&password='+
+password.value
+)
+
+.then(r=>r.json())
+.then(data=>{
+
+if(data.success){
+localStorage.user=username.value;
+showApp();
+}else{
+alert('Login gagal');
+}
+
+});
+
+}
+
+function showApp(){
+
+let user=localStorage.user;
+
+if(!user) return;
+
+loginBox.style.display='none';
+app.style.display='block';
+
+welcome.innerHTML='Halo '+user+' 👋';
+
+loadData();
+
+}
+
+function simpan(){
+
+fetch(API_URL,{
+method:'POST',
+body:JSON.stringify({
+tanggal:tanggal.value,
+user:localStorage.user,
+jenis:jenis.value,
+kategori:kategori.value,
+keterangan:keterangan.value,
+nominal:nominal.value
+})
+})
+
+.then(r=>r.json())
+.then(()=>{
+loadData();
+});
+
+}
+
+showApp();
